a8696b9b420b41ea8b69a7a13beb0b195e8f5aef
snkkid <florian.poizac@gmail.com>
2023-04-06 16:26:48 +0200

    noMsg

--- AppData/Local/nvim/lua/core/options.lua
+++ AppData/Local/nvim/lua/core/options.lua
@@ -1,6 +1,11 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+
+vim.opt.title = true
 vim.opt.backspace = '2'
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
@@ -18,3 +23,12 @@ vim.opt.expandtab = true
 vim.wo.number = true
 
 vim.cmd [[ set noswapfile ]]
+
+-- Shell options
+-- Sets the shell to use for system() and ! commands
+vim.opt.shell = 'pwsh.exe'
+vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command '
+vim.opt.shellxquote = ''
+vim.opt.shellquote = ''
+vim.opt.shellredir = '2>&1 | Out-File -Encoding UTF8 %s'
+vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s'
--- AppData/Local/nvim/lua/core/plugins.lua
+++ AppData/Local/nvim/lua/core/plugins.lua
@@ -34,7 +34,7 @@ local plugins = {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.1',
     dependencies = {{'nvim-lua/plenary.nvim'}}
-  }
+  },
 }
 
 local opts = {}
