184c3ed40e5c3923b10f8b97413a69cf0bf5d3ec
snkkid <florian.poizac@gmail.com>
2024-04-15 08:36:44 +0200

    modif racourci codium

--- AppData/Local/nvim/lua/plugins/codium.lua
+++ AppData/Local/nvim/lua/plugins/codium.lua
@@ -3,9 +3,9 @@ return {
   event = "User AstroFile",
   config = function()
     vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-    vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
-    vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
-    vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+    vim.keymap.set("i", "<C-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+    vim.keymap.set("i", "<C-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+    vim.keymap.set("i", "<C-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
     vim.keymap.set("n", "<Leader>;", function()
       if vim.g.codeium_enabled == true then
         vim.cmd "CodeiumDisable"
