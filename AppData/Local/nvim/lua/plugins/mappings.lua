646cbc970c0b1dc0f5825cca36a1cf93fb82e84d
snkkid <florian.poizac@gmail.com>
2024-07-17 17:59:07 +0200

    add mappings.lua

---
+++ AppData/Local/nvim/lua/plugins/mappings.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        -- first key is the mode
+        n = {
+          --["<Leader>vn"] = { ':execute "normal! \\<C-v><cr>"', desc = "New tab" },
+        },
+        t = {},
+      },
+    },
+  },
+}
